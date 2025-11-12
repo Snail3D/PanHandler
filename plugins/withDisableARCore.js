08a52a07ebf22ab98c178cb6774add2724b6dab0
Snail3D <109130211+Snail3D@users.noreply.github.com>
2025-11-11 21:28:51 -0700

    Update ARCore plugin to explicitly mark AR as optional
    - Instead of just removing metadata, explicitly set com.google.ar.core=optional
    - This tells Google Play that AR is not required for the app
    - More comprehensive removal of AR metadata before adding optional flag

--- plugins/withDisableARCore.js
+++ plugins/withDisableARCore.js
@@ -1,38 +1,60 @@
-const { withAndroidManifest, AndroidConfig } = require('@expo/config-plugins');
+const { withAndroidManifest } = require('@expo/config-plugins');
 
 module.exports = function withDisableARCore(config) {
   return withAndroidManifest(config, (config) => {
     const androidManifest = config.modResults;
 
-    // Remove ARCore metadata from application
-    if (androidManifest.manifest?.application?.[0]) {
-      const application = androidManifest.manifest.application[0];
-      
-      if (application['meta-data']) {
-        const originalLength = application['meta-data'].length;
-        
-        // Remove all ARCore-related metadata
-        application['meta-data'] = application['meta-data'].filter((meta) => {
-          const name = meta.$?.['android:name'];
-          return name !== 'com.google.ar.core' && 
-                 name !== 'com.google.ar.core.min_apk_version';
-        });
-        
-        console.log(`Removed ${originalLength - application['meta-data'].length} ARCore meta-data entries`);
-      }
+    // Ensure manifest structure exists
+    if (!androidManifest.manifest) {
+      return config;
     }
-
-    // Also remove from uses-feature if present
-    if (androidManifest.manifest?.['uses-feature']) {
-      const originalLength = androidManifest.manifest['uses-feature'].length;
+    
+    const manifest = androidManifest.manifest;
+    
+    // Remove ARCore from uses-feature if present
+    if (manifest['uses-feature']) {
+      const originalFeatureLength = manifest['uses-feature'].length;
       
-      androidManifest.manifest['uses-feature'] = androidManifest.manifest['uses-feature'].filter((feature) => {
+      manifest['uses-feature'] = manifest['uses-feature'].filter((feature) => {
         const name = feature.$?.['android:name'];
-        return !name?.includes('ar.core');
+        return !name?.includes('ar.core') && !name?.includes('com.google.ar');
       });
       
-      console.log(`Removed ${originalLength - androidManifest.manifest['uses-feature'].length} ARCore uses-feature entries`);
+      console.log(`[withDisableARCore] Removed ${originalFeatureLength - manifest['uses-feature'].length} ARCore uses-feature entries`);
     }
+
+    // Handle application metadata
+    if (!manifest.application) {
+      manifest.application = [{}];
+    }
+    
+    const application = manifest.application[0];
+    
+    // Remove existing ARCore metadata
+    if (application['meta-data']) {
+      const originalMetaLength = application['meta-data'].length;
+      
+      application['meta-data'] = application['meta-data'].filter((meta) => {
+        const name = meta.$?.['android:name'];
+        // Remove ALL Google AR related metadata
+        return !name?.includes('com.google.ar');
+      });
+      
+      console.log(`[withDisableARCore] Removed ${originalMetaLength - application['meta-data'].length} ARCore meta-data entries`);
+    } else {
+      application['meta-data'] = [];
+    }
+    
+    // IMPORTANT: Explicitly add ARCore as "optional" 
+    // This tells Google Play that AR is NOT required
+    application['meta-data'].push({
+      $: {
+        'android:name': 'com.google.ar.core',
+        'android:value': 'optional'
+      }
+    });
+    
+    console.log('[withDisableARCore] Added com.google.ar.core=optional metadata');
 
     return config;
   });
