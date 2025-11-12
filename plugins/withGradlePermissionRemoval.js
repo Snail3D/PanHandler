a9c3cad70513603686f68de03e2c8f57042cb9a6
Snail3D <109130211+Snail3D@users.noreply.github.com>
2025-11-12 11:22:46 -0700

    Fix Gradle plugin API compatibility issue
    - Fixed manifestOutputDirectory error with newer Android Gradle Plugin
    - Changed to use tasks.whenTaskAdded approach
    - Now uses buildDir relative paths instead of output.processManifest
    - Added removal of ACCESS_NETWORK_STATE and media permissions
    - Should work with Android Gradle Plugin 8.x

--- plugins/withGradlePermissionRemoval.js
+++ plugins/withGradlePermissionRemoval.js
@@ -12,34 +12,53 @@ android {
     }
 }
 
-// Forcefully remove permissions at build time
-afterEvaluate {
-    android.applicationVariants.all { variant ->
-        variant.outputs.each { output ->
-            output.processManifest.doLast {
-                def manifestOutFile = output.processManifest.manifestOutputDirectory.file("AndroidManifest.xml").get().asFile
-                if (manifestOutFile.exists()) {
-                    def manifestContent = manifestOutFile.getText('UTF-8')
-                    
-                    // Remove RECORD_AUDIO permission
-                    manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.RECORD_AUDIO"[^>]*/>', '')
-                    
-                    // Remove MODIFY_AUDIO_SETTINGS permission
-                    manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.MODIFY_AUDIO_SETTINGS"[^>]*/>', '')
-                    
-                    // Remove ACTIVITY_RECOGNITION permission
-                    manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.ACTIVITY_RECOGNITION"[^>]*/>', '')
-                    
-                    // Remove biometric permissions
-                    manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.USE_BIOMETRIC"[^>]*/>', '')
-                    manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.USE_FINGERPRINT"[^>]*/>', '')
-                    
-                    // Remove microphone feature
-                    manifestContent = manifestContent.replaceAll('<uses-feature[^>]*android:name="android\\\\.hardware\\\\.microphone"[^>]*/>', '')
-                    
-                    manifestOutFile.write(manifestContent, 'UTF-8')
-                    println "[PanHandler] Forcefully removed audio/biometric permissions from manifest"
-                }
+// Forcefully remove permissions at build time - Updated for newer Android Gradle Plugin
+tasks.whenTaskAdded { task ->
+    if (task.name == 'processReleaseManifest' || task.name == 'processDebugManifest') {
+        task.doLast {
+            def manifestFile = new File(buildDir, 'intermediates/merged_manifests/release/AndroidManifest.xml')
+            if (!manifestFile.exists()) {
+                manifestFile = new File(buildDir, 'intermediates/merged_manifests/debug/AndroidManifest.xml')
+            }
+            if (!manifestFile.exists()) {
+                // Try another common location
+                manifestFile = new File(buildDir, 'intermediates/manifests/full/release/AndroidManifest.xml')
+            }
+            
+            if (manifestFile.exists()) {
+                def manifestContent = manifestFile.getText('UTF-8')
+                
+                // Remove RECORD_AUDIO permission
+                manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.RECORD_AUDIO"[^>]*/>', '')
+                
+                // Remove MODIFY_AUDIO_SETTINGS permission
+                manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.MODIFY_AUDIO_SETTINGS"[^>]*/>', '')
+                
+                // Remove ACTIVITY_RECOGNITION permission
+                manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.ACTIVITY_RECOGNITION"[^>]*/>', '')
+                
+                // Remove biometric permissions
+                manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.USE_BIOMETRIC"[^>]*/>', '')
+                manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.USE_FINGERPRINT"[^>]*/>', '')
+                
+                // Remove microphone feature
+                manifestContent = manifestContent.replaceAll('<uses-feature[^>]*android:name="android\\\\.hardware\\\\.microphone"[^>]*/>', '')
+                
+                // Remove ACCESS_NETWORK_STATE permission
+                manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.ACCESS_NETWORK_STATE"[^>]*/>', '')
+                
+                // Remove all media permissions
+                manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.READ_MEDIA_AUDIO"[^>]*/>', '')
+                manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.READ_MEDIA_VIDEO"[^>]*/>', '')
+                manifestContent = manifestContent.replaceAll('<uses-permission[^>]*android:name="android\\\\.permission\\\\.READ_MEDIA_VISUAL_USER_SELECTED"[^>]*/>', '')
+                
+                // Remove CHECK_LICENSE permission
+                manifestContent = manifestContent.replaceAll('<uses-permission[^>]*com\\\\.android\\\\.vending\\\\.CHECK_LICENSE"[^>]*/>', '')
+                
+                manifestFile.write(manifestContent, 'UTF-8')
+                println "[PanHandler] Forcefully removed unwanted permissions from manifest at: " + manifestFile.path
+            } else {
+                println "[PanHandler] Warning: Could not find manifest file to clean"
             }
         }
     }
