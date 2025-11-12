b6e7094345a2f272e45846e89442716416515914
Snail3D <109130211+Snail3D@users.noreply.github.com>
2025-11-11 19:37:07 -0700

    Enhance ARCore removal plugin with logging and uses-feature removal
    - Add console logging to show what's being removed
    - Also remove ARCore from uses-feature tags
    - More aggressive filtering of ARCore metadata

--- plugins/withDisableARCore.js
+++ plugins/withDisableARCore.js
@@ -1,22 +1,37 @@
-const { withAndroidManifest } = require('@expo/config-plugins');
+const { withAndroidManifest, AndroidConfig } = require('@expo/config-plugins');
 
 module.exports = function withDisableARCore(config) {
-  return withAndroidManifest(config, async (config) => {
+  return withAndroidManifest(config, (config) => {
     const androidManifest = config.modResults;
-    const { manifest } = androidManifest;
 
-    if (!manifest.application) {
-      return config;
+    // Remove ARCore metadata from application
+    if (androidManifest.manifest?.application?.[0]) {
+      const application = androidManifest.manifest.application[0];
+      
+      if (application['meta-data']) {
+        const originalLength = application['meta-data'].length;
+        
+        // Remove all ARCore-related metadata
+        application['meta-data'] = application['meta-data'].filter((meta) => {
+          const name = meta.$?.['android:name'];
+          return name !== 'com.google.ar.core' && 
+                 name !== 'com.google.ar.core.min_apk_version';
+        });
+        
+        console.log(`Removed ${originalLength - application['meta-data'].length} ARCore meta-data entries`);
+      }
     }
 
-    const application = manifest.application[0];
-    
-    // Remove any ARCore metadata entirely
-    if (application['meta-data']) {
-      application['meta-data'] = application['meta-data'].filter(
-        (meta) => meta.$['android:name'] !== 'com.google.ar.core' && 
-                  meta.$['android:name'] !== 'com.google.ar.core.min_apk_version'
-      );
+    // Also remove from uses-feature if present
+    if (androidManifest.manifest?.['uses-feature']) {
+      const originalLength = androidManifest.manifest['uses-feature'].length;
+      
+      androidManifest.manifest['uses-feature'] = androidManifest.manifest['uses-feature'].filter((feature) => {
+        const name = feature.$?.['android:name'];
+        return !name?.includes('ar.core');
+      });
+      
+      console.log(`Removed ${originalLength - androidManifest.manifest['uses-feature'].length} ARCore uses-feature entries`);
     }
 
     return config;
